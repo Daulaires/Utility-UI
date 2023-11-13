2e927b670d163fa975ef13c84dccbcc95c0a2e66
Daulaires <102845355+Daulaires@users.noreply.github.com>
2023-11-13 13:18:15 -0600

    Update Memory.h

--- Depenencies/Headers/Memory.h
+++ Depenencies/Headers/Memory.h
@@ -2,6 +2,7 @@
 
 class Memory {
 public:
+	auto ReadMemory(DWORD address, DWORD procID) -> int;
 	auto SilentMoveFile(string name, string path, string& newpath) -> void;
 	auto SilentWriteToFile(string, string, string) -> bool;
 	auto SilentReadFile(string, string) -> bool;
